00536db63b102a1cf4c253cdba0028b4f5dd82c1
GersonJrr <mirandajunior.gm@gmail.com>
2025-12-29 13:54:35 -0300

    cores mobile

--- app/components/hero-section.jsx
+++ app/components/hero-section.jsx
@@ -14,26 +14,20 @@ export function Hero() {
         lg:flex-row
         items-center
         justify-center
-        gap-50
+        gap-28
         max-w-[1400px]
         px-6
+        bg-white
       "
     >
-      <div
-        className="
-          max-w-xl
-          text-left
-          lg:text-left
-          py-10
-        "
-      >
+      <div className="max-w-xl text-left py-10">
         <h1
-          className={`text-3xl md:text-4xl font-bold mb-4 ${space.className}`}
+          className={`text-3xl md:text-4xl font-bold mb-4 text-[#191A23] ${space.className}`}
         >
           Seu site é a vitrine para novos clientes
         </h1>
 
-        <p className="text-base md:text-lg text-gray-600">
+        <p className="text-base md:text-lg text-[#191A23]/80">
           Com um site profissional, seu negócio ganha destaque online, novas
           oportunidades de clientes e espaço para mostrar seus serviços,
           projetos e talentos online.
@@ -55,6 +49,7 @@ export function Hero() {
         </button>
       </div>
 
+      {/* IMAGEM SOMENTE DESKTOP */}
       <div className="hidden lg:block w-[450px]">
         <img src="/Frame.svg" alt="Ilustração" />
       </div>
